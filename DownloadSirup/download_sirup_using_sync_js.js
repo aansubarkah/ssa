d9775c1204386be55d9dda8f071d7851f7ad7135
Aan Subarkah <aansubarkah@gmail.com>
2024-10-16 15:01:01 +0800

    scrape sirup without any tools except browser

--- DownloadSirup/download_sirup_using_sync_js.js
+++ DownloadSirup/download_sirup_using_sync_js.js
@@ -1,4 +1,46 @@
-let banyak_data = 100000;
+let banyak_data = 50000;
+
+function loadScript(url, callback) {
+  // Create a new script element
+  var script = document.createElement("script");
+
+  // Set the source of the script
+  script.src = url;
+
+  // Set the type of the script (optional for modern browsers)
+  script.type = "text/javascript";
+
+  // Define what happens on script load
+  script.onload = function () {
+    console.log("Script loaded successfully");
+    if (callback) callback();
+  };
+
+  // Define what happens on script load error
+  script.onerror = function () {
+    console.error("Failed to load script");
+    if (callback) callback(new Error("Script load error"));
+  };
+
+  // Append the script to the head of the document
+  document.head.appendChild(script);
+}
+
+loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js",
+  function (error) {
+    if (error) {
+      console.error("Error loading script:", error);
+    } else {
+      console.log("Script loaded, XLSX object is now available");
+      // You can now use the XLSX object or any other functionality provided by the loaded script
+      // For example:
+      if (typeof XLSX !== "undefined") {
+        console.log("XLSX version:", XLSX.version);
+      }
+    }
+  }
+);
 
 //let url_api = 'https://sirup.lkpp.go.id/sirup/caripaketctr/search?tahunAnggaran=2024&jenisPengadaan=&metodePengadaan=&minPagu=&maxPagu=&bulan=&lokasi=&kldi=&pdn=&ukm=&draw=1&columns%5B0%5D%5Bdata%5D=&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=paket&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=pagu&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=jenisPengadaan&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=isPDN&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=isUMK&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=metode&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=pemilihan&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B8%5D%5Bdata%5D=kldi&columns%5B8%5D%5Bname%5D=&columns%5B8%5D%5Bsearchable%5D=true&columns%5B8%5D%5Borderable%5D=true&columns%5B8%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B8%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B9%5D%5Bdata%5D=satuanKerja&columns%5B9%5D%5Bname%5D=&columns%5B9%5D%5Bsearchable%5D=true&columns%5B9%5D%5Borderable%5D=true&columns%5B9%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B9%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B10%5D%5Bdata%5D=lokasi&columns%5B10%5D%5Bname%5D=&columns%5B10%5D%5Bsearchable%5D=true&columns%5B10%5D%5Borderable%5D=true&columns%5B10%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B10%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B11%5D%5Bdata%5D=id&columns%5B11%5D%5Bname%5D=&columns%5B11%5D%5Bsearchable%5D=true&columns%5B11%5D%5Borderable%5D=true&columns%5B11%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B11%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=5&order%5B0%5D%5Bdir%5D=DESC&start=0&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1729050502322'
 let url_api = "";
@@ -11,9 +53,14 @@ let api_url = "";
 
 let data_get = null;
 
-function fetchSync(url) {
+function fetchSync(url, headers = {}) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, false); // false makes the request synchronous
+
+  Object.keys(headers).forEach(function (key) {
+    xhr.setRequestHeader(key, headers[key]);
+  });
+
   xhr.send(null);
 
   if (xhr.status === 200) {
@@ -25,7 +72,12 @@ function fetchSync(url) {
 
 function getData(api_url) {
   try {
-    let data = fetchSync(api_url);
+    let headers = {
+      Accept: "application/json, text/javascript, */*; q=0.01",
+      "X-Requested-With": "XMLHttpRequest",
+    };
+
+    let data = fetchSync(api_url, headers);
     console.log(data);
     return data;
   } catch (error) {
@@ -78,16 +130,38 @@ function downloadCSV(jsonData, filename = "data.csv") {
   }
 }
 
-function process_data(url) {
+function downloadXLSX(jsonData, filename = "data.xlsx") {
+  csvData = jsonToCSV(jsonData);
+
+  const workbook = XLSX.utils.book_new();
+  //const worksheet = XLSX.utils.csv_to_sheet(csvData);
+  const worksheet = XLSX.utils.json_to_sheet(jsonData);
+
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+  const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+  const blob = new Blob([excelBuffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function ambil_data(url, page = 1, filename = 'sirup_data') {
+  let start_from = (page - 1) * banyak_data;
   let api_url = url
     .replace("length=10", "length=" + String(banyak_data))
-    .replace(/start=\d+/g, "start=0");
+    .replace(/start=\d+/g, "start=" + String(start_from));
 
   data_get = getData(api_url);
 
   if (data_get && data_get.data) {
     console.log("Data retrieved successfully");
-    downloadCSV(data_get.data, "sirup_data.csv");
+    downloadCSV(data_get.data, filename + ".csv");
+    downloadXLSX(data_get.data, filename + ".xlsx");
   } else {
     console.log("Failed to retrieve data");
   }
